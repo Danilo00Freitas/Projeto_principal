e9ae6a56ce992e6f7f7e7bd864edeb545cd10fe8
Danilo Freitas <danilosfreitass@gmail.com>
2024-05-17 04:42:26 -0300

    criado projeto no spring initializer

---
+++ backend/ProjetoPrincipal/src/main/java/br/com/projetoPrincipal/ProjetoPrincipal/ProjetoPrincipalApplication.java
@@ -0,0 +1,13 @@
+package br.com.projetoPrincipal.ProjetoPrincipal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoPrincipalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoPrincipalApplication.class, args);
+	}
+
+}
---
+++ backend/ProjetoPrincipal/src/test/java/br/com/projetoPrincipal/ProjetoPrincipal/ProjetoPrincipalApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.projetoPrincipal.ProjetoPrincipal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoPrincipalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
