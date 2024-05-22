10793adc9428e877dadee92e1ceb3e53e60e12f9
Danilo Freitas <danilosfreitass@gmail.com>
2024-05-22 03:54:31 -0300

    finalizando a criação dos elementos de segurança

---
+++ backend/ProjetoPrincipal/src/main/java/br/com/projetoPrincipal/ProjetoPrincipal/infra/security/CustomUserDetailsService.java
@@ -0,0 +1,21 @@
+package br.com.projetoPrincipal.ProjetoPrincipal.infra.security;
+
+import br.com.projetoPrincipal.ProjetoPrincipal.Domain.user.User;
+import br.com.projetoPrincipal.ProjetoPrincipal.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.ArrayList;
+
+public class CustomUserDetailsService implements UserDetailsService {
+   @Autowired
+   private UserRepository userRepository;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = this.userRepository.findByEmail(username).orElseThrow(()-> new UsernameNotFoundException("User not found"));
+        /*Criando o user pensando no springSecurity, que vai conter o array com as roles permitidas...*/
+        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), new ArrayList<>());
+    }
+}
--- backend/ProjetoPrincipal/src/main/java/br/com/projetoPrincipal/ProjetoPrincipal/infra/security/TokenService.java
+++ backend/ProjetoPrincipal/src/main/java/br/com/projetoPrincipal/ProjetoPrincipal/infra/security/TokenService.java
@@ -11,11 +11,12 @@ import org.springframework.stereotype.Service;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
-
+/*A classe TokenService é um serviço Spring responsável por gerar e validar tokens JWT. Ela utiliza a biblioteca Auth0 para criar tokens JWT contendo o e-mail do usuário como assunto e com um tempo de expiração de 1 hora. A geração e verificação dos tokens são realizadas utilizando o algoritmo HMAC-SHA256 com uma chave secreta definida nas propriedades da aplicação.*/
 @Service
 public class TokenService {
     @Value("${api.security.token.secret}")
     private String secret;
+    //Cria um token de verificação utilizando JWT. Enviamos o e-mail da entidade user dentro do token
     public String generateToken(User user){
         try {
             Algorithm algorithm = Algorithm.HMAC256(secret);
@@ -28,6 +29,7 @@ public class TokenService {
             throw new RuntimeException("Error while authenticating \n" + ex.getMessage());
         }
     }
+    //Realiza a validação do token gerado... capturamos o email recebido no token.
     public String validateToken(String token){
         Algorithm algorithm = Algorithm.HMAC256(secret);
         try {
@@ -45,5 +47,4 @@ public class TokenService {
         //retorna um "instant" da biblioteca time... essa quantidade de tempo representa o tempo de duração do token gerado.
         return LocalDateTime.now().plusHours(1).toInstant(ZoneOffset.of("-3"));
     }
-
 }
